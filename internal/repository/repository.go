35667f19ccaa8329c8ac1efe93e165ef6d8b4b80
Aibek_kz <pigeonthe@gmail.com>
2023-08-13 22:22:11 +0600

    finished

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -8,12 +8,13 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+//go:generate mockgen -source=repository.go -destination=mocks/mock.go
 type Task interface {
 	CreateTask(ctx context.Context, task models.InputTask, activeAt time.Time, createdAt time.Time) (string, int, error)
 	GetTasks(ctx context.Context, status string) ([]models.InputTask, error)
 	DeleteTask(ctx context.Context, id string) (int, error)
 	MarkTaskAsDone(ctx context.Context, id string) (int, error)
-	UpdateTask(ctx context.Context, updatedInput models.InputTask, id string, activeAt time.Time) (int, error) 
+	UpdateTask(ctx context.Context, updatedInput models.InputTask, id string, activeAt time.Time) (int, error)
 }
 
 type Repository struct {
